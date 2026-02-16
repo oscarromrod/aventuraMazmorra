6661bb44c7ad3eae15101b50c7c80d6f882280a6
Mohamed <simo2026kh@gmail.com>
2026-02-16 17:26:01 +0100

    Commit -> create class Heroe

---
+++ src/AventuraEnLaMazmorra/entidades/Heroe.java
@@ -0,0 +1,62 @@
+package AventuraEnLaMazmorra.entidades;
+
+import java.util.ArrayList;
+
+public class Heroe extends Personaje{
+
+    /*
+    3. Clase Heroe (hereda de Personaje) (1.5 puntos)
+    Atributos adicionales:
+    TipoHeroe tipo
+    int nivel (empieza en 1)
+    int experiencia (empieza en 0)
+    ArrayList<Item> inventario
+    Constructor:
+    Según el TipoHeroe, establece stats diferentes:
+    GUERRERO: Vida alta (100), Ataque medio (20), Defensa alta (15)
+    MAGO: Vida baja (60), Ataque alto (30), Defensa baja (5)
+    ARQUERO: Vida media (80), Ataque medio (25), Defensa media
+    (10)
+     */
+
+    //Atributos
+    private TipoHeroe tipo;
+    private Integer nivel;
+    private Integer experiencia;
+    private ArrayList<Item> inventario;
+
+
+    public Heroe(String nombre, Integer puntosVidaMax, Integer puntosVidaActual, Integer ataque, Integer defensa, boolean vivo, TipoHeroe tipo) {
+        super(nombre, puntosVidaMax, puntosVidaActual, ataque, defensa, vivo);
+        this.tipo = tipo;
+
+        this.nivel = 1;
+        this.experiencia = 0;
+        this.inventario = new ArrayList<>();
+
+    }
+
+    public TipoHeroe getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoHeroe tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getNivel() {
+        return nivel;
+    }
+
+
+    public Integer getExperiencia() {
+        return experiencia;
+    }
+
+
+    public ArrayList<Item> getInventario() {
+        return inventario;
+    }
+
+
+}
