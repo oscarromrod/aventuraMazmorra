ed2a7ffb697c58eb75378bcd23dd8dc76d5b0196
oscarromrod <oscarromera6@gmail.com>
2026-02-16 17:00:23 +0100

    personaje

--- src/AventuraEnLaMazmorra/entidades/Personaje.java
+++ src/AventuraEnLaMazmorra/entidades/Personaje.java
@@ -1,6 +1,6 @@
 package AventuraEnLaMazmorra.entidades;
 
-public class Personaje {
+public abstract class Personaje {
 
     protected String nombre;
     protected Integer puntosVidaMax;
@@ -9,4 +9,79 @@ public class Personaje {
     protected Integer defensa;
     protected boolean vivo;
 
+    public Personaje(String nombre, Integer puntosVidaMax, Integer puntosVidaActual, Integer ataque, Integer defensa, boolean vivo) {
+        this.nombre = nombre;
+        this.puntosVidaMax = puntosVidaMax;
+        this.puntosVidaActual = puntosVidaActual;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.vivo = vivo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getPuntosVidaMax() {
+        return puntosVidaMax;
+    }
+
+    public void setPuntosVidaMax(Integer puntosVidaMax) {
+        this.puntosVidaMax = puntosVidaMax;
+    }
+
+    public Integer getPuntosVidaActual() {
+        return puntosVidaActual;
+    }
+
+    public void setPuntosVidaActual(Integer puntosVidaActual) {
+        this.puntosVidaActual = puntosVidaActual;
+    }
+
+    public Integer getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(Integer defensa) {
+        this.defensa = defensa;
+    }
+
+    public Integer getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(Integer ataque) {
+        this.ataque = ataque;
+    }
+
+    public boolean isVivo() {
+        return vivo;
+    }
+
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Personaje{");
+        sb.append("nombre='").append(nombre).append('\'');
+        sb.append(", puntosVidaMax=").append(puntosVidaMax);
+        sb.append(", puntosVidaActual=").append(puntosVidaActual);
+        sb.append(", ataque=").append(ataque);
+        sb.append(", defensa=").append(defensa);
+        sb.append(", vivo=").append(vivo);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    //---------------METODOS-----------------
+
+    public void usarHabilidadEspecial(Personaje objetivo) {
+
+    }
 }
