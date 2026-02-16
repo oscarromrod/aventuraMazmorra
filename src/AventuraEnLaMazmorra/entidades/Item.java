5007bd36f6abd6a4b163c62b25a60a5295d30358
DiezCano <jdiecan2926@gmail.com>
2026-02-16 17:50:10 +0100

    Enum

--- src/AventuraEnLaMazmorra/entidades/Item.java
+++ src/AventuraEnLaMazmorra/entidades/Item.java
@@ -6,25 +6,58 @@ public class Item {
     private TipoItem tipo;
     private int valorCuracion;
 
-    public Item(String nombre,TipoItem tipo,int valorCuracion) {
+    public Item(String nombre,TipoItem tipo) {
 
         this.nombre = nombre;
         this.tipo = tipo;
-        this.valorCuracion = valorCuracion;
 
-        if (tipo == TipoItem.POCION_PEQUENA) {
-            this.nombre = "Poción Pequeña";
-            this.valorCuracion = 30;
-
-        } else if (tipo == TipoItem.POCION_GRANDE) {
-            this.nombre = "Poción Grande";
-            this.valorCuracion = 60;
-
-        } else {
-            this.nombre = "Elixir";
-            this.valorCuracion = -1;
-        }
     }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Item{");
+        sb.append("nombre='").append(nombre).append('\'');
+        sb.append(", tipo=").append(tipo);
+        sb.append(", valorCuracion=").append(valorCuracion);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public TipoItem getTipo() {
+        return tipo;
+    }
+
+    public int getValorCuracion() {
+        return valorCuracion;
+    }
+
+    //------Metodos------
+
+   // public void usar(Heroe heroe) {
+
+        //if (tipo == TipoItem.POCION_PEQUENA) {
+            //valorCuracion = 30;
+            //heroe.curar(valorCuracion);
+            //System.out.println(heroe.getNombre() + " usa una Poción Pequeña y recupera 30 HP.");
+
+       // } else if (tipo == TipoItem.POCION_GRANDE) {
+            //valorCuracion = 60;
+            //heroe.curarItem(valorCuracion);
+            //System.out.println(heroe.getNombre() + " usa una Poción Grande y recupera 60 HP.");
+
+       // } else if (tipo == TipoItem.ELIXIR) {
+            //int vidaFaltante = heroe.getPuntosVidaMax() - heroe.getPuntosVidaActual();
+           // heroe.curarItem(vidaFaltante);
+           // System.out.println(heroe.getNombre() + " usa un Elixir y se cura completamente.");
+        //}
+    //}
+
+
+
 
 
 }
---
+++ src/AventuraEnLaMazmorra/entidades/Sala.java
@@ -0,0 +1,12 @@
+package AventuraEnLaMazmorra.entidades;
+
+import java.util.ArrayList;
+
+public class Sala {
+
+    private int numeroSala;
+    private ArrayList<Enemigo> enemigos;
+
+
+
+}
