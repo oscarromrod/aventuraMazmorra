366ecae927a3db9bd496c384eadfc2140b3913ff
oscarromrod <oscarromera6@gmail.com>
2026-02-16 17:40:29 +0100

    personaje

--- src/AventuraEnLaMazmorra/entidades/Enemigo.java
+++ src/AventuraEnLaMazmorra/entidades/Enemigo.java
@@ -5,12 +5,9 @@ public class Enemigo extends Personaje{
     private TipoEnemigo tipo;
     private Integer expOtorgada;
 
-    public Enemigo(String nombre, Integer puntosVidaMax, Integer puntosVidaActual, Integer ataque, Integer defensa, boolean vivo, TipoEnemigo tipo, Integer expOtorgada) {
+    public Enemigo(String nombre, Integer puntosVidaMax, Integer puntosVidaActual, Integer ataque, Integer defensa, boolean vivo, Integer expOtorgada, TipoEnemigo tipo) {
         super(nombre, puntosVidaMax, puntosVidaActual, ataque, defensa, vivo);
-        if (this.tipo == TipoEnemigo.GOBLIN){
-
-        }
-        this.tipo = tipo;
         this.expOtorgada = expOtorgada;
+        this.tipo = tipo;
     }
 }
