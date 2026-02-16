c951215f23ae9f8bff312886e59ae80c38930c81
oscarromrod <oscarromera6@gmail.com>
2026-02-16 16:39:03 +0100

    cmmit inicial

---
+++ src/AventuraEnLaMazmorra/entidades/Personaje.java
@@ -0,0 +1,12 @@
+package AventuraEnLaMazmorra.entidades;
+
+public class Personaje {
+
+    protected String nombre;
+    protected Integer puntosVidaMax;
+    protected Integer puntosVidaActual;
+    protected Integer ataque;
+    protected Integer defensa;
+    protected boolean vivo;
+
+}
