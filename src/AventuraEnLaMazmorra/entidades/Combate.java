8dd5c59e67209e98304c6db67d460e8e01d81ca6
DiezCano <jdiecan2926@gmail.com>
2026-02-17 18:17:23 +0100

    Enum

---
+++ src/AventuraEnLaMazmorra/entidades/Combate.java
@@ -0,0 +1,23 @@
+package AventuraEnLaMazmorra.entidades;
+
+import java.util.ArrayList;
+
+public class Combate {
+    private ArrayList<Heroe> heroes;
+    private Sala sala;
+    private int turno;
+
+    public Combate(ArrayList<Heroe> heroes, Sala sala) {
+        this.heroes = heroes;
+        this.sala = sala;
+        this.turno = 1;
+    }
+
+
+
+
+
+
+
+
+}
